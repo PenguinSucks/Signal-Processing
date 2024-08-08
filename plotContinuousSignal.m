9b6bf54f7b4d40a3a9a88ec9c24b711daac19b56
Akhil <144369179+AkhilRai28@users.noreply.github.com>
2024-08-09 00:23:01 +0530

    Update plotContinuousSignal.m

--- plotContinuousSignal.m
+++ plotContinuousSignal.m
@@ -3,5 +3,5 @@ function plotContinuousSignal(time, signal, titleStr)
     title(titleStr);
     xlabel('Time');
     ylabel('Amplitude');
-    grid on; % Optional: Add grid for better visualization
+    grid on; % Optional
 end
